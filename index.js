01363ba7c7bd294b2d54186f520dd0e51f01f1ac
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-11-16 14:44:05 +0800

    challenge finished

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
@@ -8,15 +5,38 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express;
+const app = express();
 const port = 3000;
+var isAuthenticated = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// function passwordCheck(req, res, next) {
+//   const password = req.body["password"];
+//   if (password === "ILoveProgramming") {
+//     isAuthenticated = true;
+//   }
+//   next();
+// }
+
+// app.use(passwordCheck);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+app.post("/check", (req, res) => {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    isAuthenticated = true;
+  }
+  if (isAuthenticated) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
